701d6e1984a0653b5067d1461af06e0d52752bb5
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2022-06-07 21:59:06 -0500

    while

--- js/app.js
+++ js/app.js
@@ -1,16 +1,11 @@
 "use strict"
 
-"use strict"
-
-var y = 5;
+var num = 5;
+var i = 0;
 var texto = "";
 
-do{
-    for (let x = 0; x < y; x++) {
-        texto = texto + "*";
-    }
-    console.log(texto)
-    texto = "";
-    y--;
-} 
-while (y>0);
+while (i < num) {
+    texto = texto + "*";
+    console.log(texto);
+    i ++;
+}
